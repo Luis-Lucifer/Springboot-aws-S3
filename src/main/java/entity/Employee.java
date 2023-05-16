976dae5291f655c06512fe52919bb5adfcc7bb0f
Luis-Lucifer <nilanjandas5498@gmail.com>
2023-05-16 13:08:52 +0530

    springboot with aws S3 and mysql

--- src/main/java/entity/Employee.java
+++ src/main/java/entity/Employee.java
@@ -5,20 +5,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
+@Table(name="employee")
 public class Employee {
 
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
+
+    @Column(name="emp_name")
     private String name;
+    @Column(name="email")
     private String email;
+    @Column(name="phoneNo")
     private String phoneNo;
+    @Column(name="department")
     private String department;
+    @Column(name="resumeLink")
     private String resumeLink;
 
 //    public String getResumeLink() {
